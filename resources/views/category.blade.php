25aa651aabb79e2e0e5d3d1fb6bd025b140fe9b2
Oudom3 <boloudom15@gmail.com>
2023-04-30 21:18:04 +0700

    Continue Category page

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,38 +1,116 @@
 @extends('layout.master')
 
 @section('dyncontent')
-
-<!-- <h1>nothing yet</h1> -->
-
 <div class="container">
 
+    <h2 class="text-center pb-3">Categories</h2>
     <div class="row d-flex justify-content-center">
-        <div class="col-lg-3 col-md-4 col-12">
-            <div class="card mb-3"> 
-                <div class="card-body p-3 card-head-bgcolor">
-                        <div class="d-flex justify-content-between">
-                            <h6 class="m-0">Category</h6>
-                        </div>
-                </div>
+       <div class="col-md-9">
+            <div class="accordion accordion-flush" id="Categories">
+                <div class="accordion-item acc-item-bmargin">
+                    <h2 class="accordion-header">
+                        <!-- Category name -->
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            Automotive - Vehicles
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse">
+                        <div class="accordion-body">
+                            <div class="row">
+                                <!-- Subcategories -->
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="Automobile Glass Coating" class="text-decoration-none">Automobile Glass Coating</a>
+                                </div>
 
-                <div class="card-body py-3">
-                    <a href="#" title="Automotive - Vehicles">
-                        <div class="d-flex">
-                            <span class="mb-2 px-1">Automotive - Vehicles</span>
-                        </div>
-                    </a>
-                    <a href="#" title="Banking & Finance">
-                        <div class="d-flex">
-                            <span class="mb-2 px-1">Banking & Finance</span>
-                        </div>
-                    </a>
-                   
-                </div>
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="Automotive Maintenance Equipment" class="text-decoration-none">Automotive Maintenance Equipment</a>
+                                </div>
 
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="Battery Supplies" class="text-decoration-none">Battery Supplies</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="Bulldozer" class="text-decoration-none">Bulldozer</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+                                 <!-- End Subcategories -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="accordion-item acc-item-bmargin">
+                    <h2 class="accordion-header">
+                        <!-- Category name -->
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                             Banking & Finance
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse">
+                        <div class="accordion-body">
+                            <div class="row">
+                                <!-- Subcategories -->
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="Banking Equipment & Supplies" class="text-decoration-none">Banking Equipment & Supplies</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+                                 <!-- End Subcategories -->
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+
+                <div class="accordion-item acc-item-bmargin">
+                    <h2 class="accordion-header">
+                        <!-- Category name -->
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Business Services
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse">
+                        <div class="accordion-body">
+                            <div class="row">
+                                <!-- Subcategories -->
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+
+                                <div class="col-12 col-md-4 subcate-list-bmargin">
+                                    <a href="#" title="D" class="text-decoration-none">D</a>
+                                </div>
+                                 <!-- End Subcategories -->
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+ 
             </div>
-        </div>
-    </div>  
-
+              
+       </div>
+   </div> 
 </div>
 
 @stop
