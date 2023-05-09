1c469653b4703d0dd57200e5055361ee7f2ac393
Oudom3 <boloudom15@gmail.com>
2023-05-09 21:19:46 +0700

    work abit on editing the homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,12 +10,49 @@
       <a href="/categoryshow" class="btn btn-primary btn-sm">View More</a>
     </div>
   </div>
+
+  <!-- <div class="row row-cols-1 row-cols-md-3 g-4"> -->
   <div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col ">
+    <!-- first category -->
+    <!-- <div class="col ">
       <div class="card h-100">
         <div class="card-body d-flex flex-column justify-content-center">
           <a href="#" class="text-decoration-none">
-            <h5 class="card-title mb-0">Accountants - General</h5>
+              <h5 class="card-title mb-0"> Business Services </h5>
+          </a>
+        </div>
+      </div>
+    </div> -->
+    <div class="col">
+      <div class="card h-100">
+        <img src="https://dummyimage.com/400x200/000/fff" class="card-img-top" alt="Business Services">
+        <div class="card-body d-flex flex-column justify-content-center">
+          <a href="#" class="text-decoration-none">
+            <h5 class="card-title mb-2">Business Services</h5>
+            <p class="card-subtitle text-muted">Get help with your business needs</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- end first category -->
+
+    <!-- 2nd category -->
+    <div class="col">
+      <div class="card h-100">
+        <div class="card-body d-flex flex-column justify-content-center">
+          <a href="#" class="text-decoration-none">
+            <h5 class="card-title mb-0">Banking & Finance</h5>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- End 2nd category -->
+
+    <div class="col">
+      <div class="card h-100">
+        <div class="card-body d-flex flex-column justify-content-center">
+          <a href="#" class="text-decoration-none">
+            <h5 class="card-title mb-0">Construction</h5>
           </a>
         </div>
       </div>
@@ -24,7 +61,7 @@
       <div class="card h-100">
         <div class="card-body d-flex flex-column justify-content-center">
           <a href="#" class="text-decoration-none">
-            <h5 class="card-title mb-0">Architectural - Design</h5>
+            <h5 class="card-title mb-0">Home & Household</h5>
           </a>
         </div>
       </div>
@@ -33,7 +70,7 @@
       <div class="card h-100">
         <div class="card-body d-flex flex-column justify-content-center">
           <a href="#" class="text-decoration-none">
-            <h5 class="card-title mb-0">Consultants</h5>
+            <h5 class="card-title mb-0">Industry, Agricultural & Garment</h5>
           </a>
         </div>
       </div>
@@ -42,25 +79,7 @@
       <div class="card h-100">
         <div class="card-body d-flex flex-column justify-content-center">
           <a href="#" class="text-decoration-none">
-            <h5 class="card-title mb-0">Banks & Finance</h5>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <div class="card-body d-flex flex-column justify-content-center">
-          <a href="#" class="text-decoration-none">
-            <h5 class="card-title mb-0">Pest Control Services</h5>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <div class="card-body d-flex flex-column justify-content-center">
-          <a href="#" class="text-decoration-none">
-            <h5 class="card-title mb-0">Printing Houses</h5>
+            <h5 class="card-title mb-0">Travel & Tourism</h5>
           </a>
         </div>
       </div>
