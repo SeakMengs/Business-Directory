0c7377f818a366962be6016feb39ab912fc9875e
Yato <hseakmeng22@gmail.com>
2024-02-19 22:50:46 +0700

    add database read write to different db host

--- config/database.php
+++ config/database.php
@@ -46,7 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // use read and write host if specified, otherwise use default host
+            'host' => [
+                'read' => env('DB_READ_HOST', env('DB_HOST', '127.0.0.1')),
+                'write' => env('DB_WRITE_HOST', env('DB_HOST', '127.0.0.1')),
+            ],
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
